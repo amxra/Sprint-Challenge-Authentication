cfae5d8f3d4b66abd79911dc1f4678c703f8a2de
amxra <amiradediran@gmail.com>
2019-11-15 10:29:35 +0100

    Create helper functions

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,3 +1,29 @@
 const db = require('../database/dbConfig');
 
-module.export = {add, find, findBy, findById, Remove}
+module.export = {add, find, findBy, findById, remove}
+
+function find (){
+    return db('users').select('id', 'username', 'password')
+}
+
+function findBy (filter){
+    return db('users').where(filter)
+}
+
+function add (user){
+    return db('users').insert(user, 'id')
+    .then(ids => {
+        const [id] = ids
+        return findById(id)
+    })
+}
+
+function findById(id) {
+    return db('users').where({id})
+    .first();
+}
+
+function remove (){
+    return db('user').where({id:id}).del()
+}
+
