3bbdc178708a9ee8ad89dc1ed6cab5f63e3ead19
amxra <amiradediran@gmail.com>
2019-11-15 10:31:07 +0100

    Import bcrypt and jwt

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 router.post('/register', (req, res) => {
   // implement registration
+
   
 });
 
